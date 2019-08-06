e0a2c982bd7b3feb77f307ddfe7fb61bae23ce25
Depish <reactor.game@gmail.com>
2019-08-06 20:01:48 +0300

    use submodules

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import configparser
+import sqlite3
 import os
+
+import folder_tools as ft
+import db_tools as db
 
 ## Config
 conf = configparser.ConfigParser()
 conf.read("./config/config.conf")
 
-backup_sources = str(conf['Folders']['source'])
+## Variables
+backup_sources = str(conf['Folders']['source']).replace("'", '').split(';')
+sources_count = len(backup_sources)
+
+backup_destination = str(conf['Folders']['dest']).replace("'", '').split(';')
+destination_count = len(backup_destination)
+
+## Sqlite3
+conn = db.CreateDB('initdb.sql')
